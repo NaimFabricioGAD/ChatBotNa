3928b8f33328463481ac32b09c277023d5a8c34d
Alejandro <alejandrob1519@gmail.com>
2024-03-31 17:35:48 -0500

    mejoras en variables de entorno, se agrego servicio enviar voucher, me mejoro codigo

--- src/flow/servicios.flow.js
+++ src/flow/servicios.flow.js
@@ -16,35 +16,38 @@ const axios = require("axios").default;
 // INI INICIALIZANDO VARIABLES
 //=============================================================================
 const TOKEN_API_SACPRO = process.env.TOKEN_API_SACPRO;
+const BASE_URL_API = process.env.BASE_URL_API;
 
 const API_SERVICIOS = {
-	1: "https://ccpapurimac.sacpro.pe/intranet/public/api/agremiado?servicio=habilidad&codigo=",
-	2: "https://ccpapurimac.sacpro.pe/intranet/public/api/agremiado?servicio=ultimoAporte&codigo=",
-	3: "https://ccpapurimac.sacpro.pe/intranet/public/api/agremiado?servicio=ultimaConstancia&codigo=",
-	4: "https://ccpapurimac.sacpro.pe/intranet/public/api/agremiado?servicio=deudas&codigo=",
-	5: "https://ccpapurimac.sacpro.pe/intranet/public/api/colegio?servicio=actividades",
-	6: "https://ccpapurimac.sacpro.pe/intranet/public/api/colegio?servicio=requisitos",
-	7: "https://ccpapurimac.sacpro.pe/intranet/public/api/colegio?servicio=cuentasBancarias",
-	8: "https://ccpapurimac.sacpro.pe/intranet/public/api/chatbot",
-	9: "https://ccpapurimac.sacpro.pe/intranet/public/api/chatbot_encuesta",
-	10: "https://ccpapurimac.sacpro.pe/intranet/public/api/agremiado?servicio=celular&codigo=",
+	1: BASE_URL_API + "/api/agremiado?servicio=habilidad&codigo=",
+	2: BASE_URL_API + "/api/agremiado?servicio=ultimoAporte&codigo=",
+	3: BASE_URL_API + "/api/agremiado?servicio=ultimaConstancia&codigo=",
+	4: BASE_URL_API + "/api/agremiado?servicio=deudas&codigo=",
+	5: BASE_URL_API + "/api/colegio?servicio=actividades",
+	6: BASE_URL_API + "/api/colegio?servicio=requisitos",
+	7: BASE_URL_API + "/api/colegio?servicio=cuentasBancarias",
+	8: BASE_URL_API + "/api/colegio?servicio=enviarVoucher",
+	97: BASE_URL_API + "/api/agremiado?servicio=celular&codigo=",
+	98: BASE_URL_API + "/api/chatbot",
+	99: BASE_URL_API + "/api/chatbot_encuesta"
 };
 
-let opciones1 = ["0:Consultar otro colegiado"];
+let opciones1 = ["👉 *0:* Consultar otro colegiado"];
 
 let opciones2 = [
-	"1: Consultar habilidad",
-	"2: Consultar último aporte",
-	"3: Última constancia de habilidad",
-	"4: Consultar deudas",
+	"👉 *A:* Consultar habilidad",				//1
+	"👉 *B:* Consultar último aporte",			//2
+	"👉 *C:* Última constancia de habilidad",	//3
+	"👉 *D:* Consultar deudas",					//4
 ];
 
 let opciones3 = [
-	"5: Consultar próximas actividades",
-	"6: Requisitos de colegiatura",
-	"7: Cuentas bancarias",
-	"8: Contactar con secretaría",
-	"9: Salir",
+	"👉 *E:* Consultar próximas actividades",	//5
+	"👉 *F:* Requisitos de colegiatura",			//6
+	"👉 *G:* Cuentas bancarias",					//7
+	"👉 *H:* Enviar voucher de pago",			//8
+	"👉 *I:* Contactar con secretaría",			//9
+	"👉 *J:* Salir",								//10
 ];
 //FIN INICIALIZANDO VARIABLES
 
@@ -97,7 +100,7 @@ function grabarLogChatBot(
 		redirect: "follow",
 	};
 
-	fetch(API_SERVICIOS[8], requestOptions);
+	fetch(API_SERVICIOS[98], requestOptions);
 }
 
 function grabarLogChatBotEncuesta(fuiUtil = "", celularValor = "") {
@@ -115,7 +118,7 @@ function grabarLogChatBotEncuesta(fuiUtil = "", celularValor = "") {
 		redirect: "follow",
 	};
 
-	fetch(API_SERVICIOS[9], requestOptions);
+	fetch(API_SERVICIOS[99], requestOptions);
 }
 //FIN DEFINIENDO FUNCIONES
 
@@ -210,7 +213,7 @@ const flowServicio5 = addKeyword(["###_FLOW_SERVI5_###"]).addAnswer(
 );
 
 const flowServicio6 = addKeyword(["###_FLOW_SERVI6_###"]).addAnswer(
-	"🤗ingresa al siguiente enlace para conocer los requisitos de colegiatura 😉",
+	"Ingresa al siguiente enlace para conocer los requisitos de colegiatura 😉",
 	null,
 	async (ctx, { flowDynamic, gotoFlow }) => {
 		let respuestaPersonalizada = await solicitudAxios("requisitos", "", "6");
@@ -237,8 +240,20 @@ const flowServicio7 = addKeyword(["###_FLOW_SERVI7_###"]).addAnswer(
 	}
 );
 
+const flowServicio8 = addKeyword(["###_FLOW_SERVI8_###"]).addAnswer(
+	"Ingresa al siguiente enlace para enviar tu voucher 😉",
+	null,
+	async (ctx, { flowDynamic, gotoFlow }) => {
+		let respuestaPersonalizada = await solicitudAxios("enviarVoucher", "", "8");
+
+		await flowDynamic(respuestaPersonalizada);
+		grabarLogChatBot("enviarVoucher", ctx.from);
+		return gotoFlow(flowContinuar);
+	}
+);
+
 const flowVacio = addKeyword("####_VACIOOOOOOO_###").addAnswer(
-	"No te he entendido!🤔, porfavor escriba un número de la lista",
+	"No te entendí🤔. Porfavor, selecciona un número de la lista:",
 	null,
 	async (ctx, { gotoFlow }) => {
 		return gotoFlow(flowBienvenida, 2);
@@ -264,20 +279,20 @@ const flowDespedida = addKeyword("###_FLOW_SERVI7_###")
 		"Gracias por contactarnos, espero haberte ayudado con tu consulta",
 	])
 	.addAnswer(
-		"Si te fui util, escribe *Si* o de lo contrario *No*",
+		["Si te fui util calificame con:", "1: ✅Si", "2: 🛑No"],
 		{ capture: true },
 		async (ctx, { endFlow, flowDynamic }) => {
 			let respuesta = ctx.body.toLowerCase().trim();
 			let despedidaFinal = "";
-			if (respuesta == "si") {
+			if (respuesta == "si" || respuesta == "1") {
 				despedidaFinal =
-					"Me da gusto haberte ayudado 😌, hasta la próxima y que tengas un lindo día 😉";
-				grabarLogChatBotEncuesta(ctx.body, ctx.from, ctx.body);
+					"Me da gusto haberte ayudado 😌. Hasta la próxima! y que tengas un día maravilloso 😉";
+				grabarLogChatBotEncuesta("si", ctx.from);
 			}
-			if (respuesta == "no") {
+			if (respuesta == "no" || respuesta == "2") {
 				despedidaFinal =
-					"Espero mejorar pronto 😢 con mas servicios, hasta la próxima y que tengas un lindo día 😉";
-				grabarLogChatBotEncuesta(ctx.body, ctx.from, ctx.body);
+					"Espero ser más útil pronto para ti, estoy en mejora continua. Hasta la próxima! y que tengas un día maravilloso 😉";
+				grabarLogChatBotEncuesta("no", ctx.from);
 			}
 
 			await flowDynamic(despedidaFinal);
@@ -290,7 +305,7 @@ const flowSecretariaVacio = addKeyword("___###____")
 	.addAnswer(
 		"😉👌A continuación te contactaremos con nuestro personal, espere porfavor.."
 	)
-	.addAnswer("Recuerda escribir *BOT* para volver a hablar con *GremIA*")
+	.addAnswer("Recuerda escribir *BOT* para volver a hablar la asistente virtual")
 	.addAction({ capture: true }, async (ctx, { gotoFlow }) => {
 		if (ctx.body.toLowerCase().trim() == "bot") {
 			return gotoFlow(flowBienvenida);
@@ -301,37 +316,63 @@ const flowSecretariaVacio = addKeyword("___###____")
 
 const flowBienvenida = addKeyword([EVENTS.WELCOME])
 	.addAnswer([
-		"BIENVENIDO, gracias por contactar al Colegio de Contadores Públicos de Apurímac",
+		"¡Hola! Soy la asistente virtual del " + process.env.NOMBRE_CLIENTE + " 🤖",
 	])
 	.addAnswer(
-		"Tenemos los siguientes servicios para ti, escribe el número que desees 🫡"
-	)
-	.addAnswer(
-		opciones2.concat(opciones3),
+		["Elige una de estas alternativas para poder ayudarte:"].concat(
+			opciones2.concat(opciones3)
+		),
 		{ capture: true },
 		async (ctx, { gotoFlow }) => {
-			if (ctx.body === "1") {
-				return gotoFlow(flowServicio1);
-			} else if (ctx.body === "2") {
-				return gotoFlow(flowServicio2);
-			} else if (ctx.body === "3") {
-				return gotoFlow(flowServicio3);
-			} else if (ctx.body === "4") {
-				return gotoFlow(flowServicio4);
-			} else if (ctx.body === "5") {
-				return gotoFlow(flowServicio5);
-			} else if (ctx.body === "6") {
-				return gotoFlow(flowServicio6);
-			} else if (ctx.body === "7") {
-				return gotoFlow(flowServicio7);
-			} else if (ctx.body === "8") {
-				grabarLogChatBot("Atención secretaria", ctx.from);
-				return gotoFlow(flowSecretariaVacio);
-			} else if (ctx.body === "9") {
-				return gotoFlow(flowDespedida);
-			} else {
+			
+			let opcionSeleccionada = ctx.body.toUpperCase().trim();
+
+			let resumenServicios = {
+				A: flowServicio1,
+				B: flowServicio2,
+				C: flowServicio3,
+				D: flowServicio4,
+				E: flowServicio5,
+				F: flowServicio6,
+				G: flowServicio7,
+				H: flowServicio8,
+				I: () => {
+					grabarLogChatBot("Contactar secretaria", ctx.from);
+					return gotoFlow(flowSecretariaVacio);
+				},
+				J: flowDespedida
+			};
+
+			if(resumenServicios.hasOwnProperty(opcionSeleccionada)){
+				return gotoFlow(resumenServicios[opcionSeleccionada]);
+			}else{
 				return gotoFlow(flowVacio);
 			}
+
+			// if (opcionSeleccionada === "1") {
+			// 	return gotoFlow(resumenServicios.A);
+			// } else if (opcionSeleccionada === "2") {
+			// 	return gotoFlow(resumenServicios.B);
+			// } else if (opcionSeleccionada === "3") {
+			// 	return gotoFlow(resumenServicios.C);
+			// } else if (opcionSeleccionada === "4") {
+			// 	return gotoFlow(resumenServicios.D);
+			// } else if (opcionSeleccionada === "5") {
+			// 	return gotoFlow(resumenServicios.E);
+			// } else if (opcionSeleccionada === "6") {
+			// 	return gotoFlow(resumenServicios.F);
+			// } else if (opcionSeleccionada === "7") {
+			// 	return gotoFlow(resumenServicios.G);
+			// } else if (opcionSeleccionada === "8") {
+			// 	return gotoFlow(resumenServicios.H);
+			// } else if (opcionSeleccionada === "9") {
+			// 	grabarLogChatBot("Contactar secretaria", ctx.from);
+			// 	return gotoFlow(flowSecretariaVacio);
+			// } else if (opcionSeleccionada === "10") {
+			// 	return gotoFlow(flowDespedida);
+			// } else {
+			// 	return gotoFlow(flowVacio);
+			// }
 		}
 	);
 
@@ -346,6 +387,7 @@ module.exports = {
 	flowServicio5,
 	flowServicio6,
 	flowServicio7,
+	flowServicio8,
 	flowVacio,
 	flowContinuar,
 	flowDespedida,
